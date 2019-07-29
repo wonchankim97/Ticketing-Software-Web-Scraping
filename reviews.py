ea043a1c64c47fb2a2dae184ff9119656048fc09
Wonchan Kim <wonchankim97@gmail.com>
2019-07-29 19:31:19 -0400

    got rid of useless imports in urls, read in csv properly in reviews file

--- reviews.py
+++ reviews.py
@@ -1,21 +1,25 @@
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# import csv
-# import re
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import csv
+import re
+
+urls = []
+with open('urls.csv', 'r') as f:
+    lines = f.readlines()
+    lines = list(map(lambda x: x.strip('\n'), lines))
+    for line in lines:
+        urls.append(line)
+
+print(urls)
 
 # chromeOptions = webdriver.ChromeOptions()
 # prefs = {'profile.managed_default_content_settings.images':2}
 # chromeOptions.add_experimental_option("prefs", prefs)
 # driver = webdriver.Chrome(chrome_options=chromeOptions)
 
-# # driver = webdriver.Chrome()
-
 # driver.get("https://www.capterra.com/ticketing-software/")
-
-# sort_button = driver.find_element_by_xpath('//*[@id="sort_options_select"]/option[3]')
-# sort_button.click()
 
 # # csv_file = open('reviews.csv', 'w', encoding='utf-8', newline='')
 # # writer = csv.writer(csv_file)
--- urls.py
+++ urls.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.by import By
 import csv
-import re
 
 chromeOptions = webdriver.ChromeOptions()
 prefs = {'profile.managed_default_content_settings.images':2}
