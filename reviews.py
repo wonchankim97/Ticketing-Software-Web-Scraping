f1cd5d82c72af0868e5d5d52d885151cf0e1c9fb
Wonchan Kim <wonchankim97@gmail.com>
2019-08-02 14:47:50 -0400

    fixed relative paths for for loop iteration. still working on reviews

--- reviews.py
+++ reviews.py
@@ -36,46 +36,49 @@ for url in urls[23:24]:
     while True:
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         try:
-            sleep(1.5)
-            btn = driver.find_element_by_xpath('//a[@class="no-underline  show-more-reviews"]')
+            wait_button = WebDriverWait(driver, 10)
+            btn = wait_button.until(EC.element_to_be_clickable((By.XPATH, '//a[@class="no-underline  show-more-reviews"]')))
             btn.click()
-            # ActionChains(driver).move_to_element(btn).click(btn).perform()
+            # sleep(1.5)
+            # btn = driver.find_element_by_xpath('//a[@class="no-underline  show-more-reviews"]')
+            # btn.click()
         except Exception as e:
             print('Click Error: ', e)
             break
     
-    # categories found by following paths
-    titles = driver.find_elements_by_xpath('//h3[@class="delta  weight-bold  half-margin-bottom"]/q')
-    names = driver.find_elements_by_xpath('//div[@class="epsilon  weight-bold  inline-block"]')
-    positions = driver.find_elements_by_xpath('//div[@class="opacity-threequarters"]')
-    industries = driver.find_elements_by_xpath('//div[@class="italic  opacity-threequarters"]')
-    usages = driver.find_elements_by_xpath('//div[@class="reviewer-details"]/div[5]')
-    paid_statuses = driver.find_elements_by_xpath('//span[@class="help-tooltip text-left incentive"]')
-        #.get_attribute('data-incentive-code') when looping through
-    sources = driver.find_elements_by_xpath('//div[@class="reviewer-details"]/div[7]')
-    dates = driver.find_elements_by_xpath('//div[@class="quarter-margin-bottom  micro  color-gray  weight-normal  text-right  palm-text-left"]')
+    reviews = driver.find_elements_by_xpath('//div[@class="cell-review"]')
+    for review in reviews:
+        # categories found by following paths
+        title = review.find_element_by_xpath('.//h3[@class="delta  weight-bold  half-margin-bottom"]/q').text
+        name = review.find_element_by_xpath('.//div[@class="epsilon  weight-bold  inline-block"]').text
+        position = review.find_element_by_xpath('.//div[@class="opacity-threequarters"]').text
+        industry = review.find_element_by_xpath('.//div[@class="italic  opacity-threequarters"]').text
+        usage = review.find_element_by_xpath('.//div[@class="reviewer-details"]/div[5]').text
+        paid_status = review.find_element_by_xpath('.//span[@class="help-tooltip text-left incentive"]').get_attribute('data-incentive-code')
+            #.get_attribute('data-incentive-code') when looping through
+        source = review.find_element_by_xpath('.//div[@class="reviewer-details"]/div[7]').text
+        date = review.find_element_by_xpath('.//div[@class="quarter-margin-bottom  micro  color-gray  weight-normal  text-right  palm-text-left"]').text
 
-    overalls = driver.find_elements_by_xpath('//span[@class="overall-rating"]/span')
-    # these next four are annoying because some people do not do them and the class tags are same
-    # jk, i just didn't look hard enough for the path whoohooo
-    eases = driver.find_elements_by_xpath('//span[@class="reviews-stars  rating-ease-of-use"]/span[@class="milli  rating-decimal"]/span[1]')
-    features = driver.find_elements_by_xpath('//span[@class="reviews-stars  rating-features"]/span[@class="milli  rating-decimal"]/span[1]')
-    supports = driver.find_elements_by_xpath('//span[@class="reviews-stars  rating-customer-service"]/span[@class="milli  rating-decimal"]/span[1]')
-    values = driver.find_elements_by_xpath('//div[@class="cell  three-twelfths  reviews-col columns4 lap-three-twelfths  palm-one-half"]/span[@class="reviews-stars  rating-value"]/span[@class="milli  rating-decimal"]/span[1]')
-    recommendations = driver.find_elements_by_xpath('//img[@class="gauge-svg-image"]')
-        # .get_attribute('alt')
+        overall = review.find_element_by_xpath('.//span[@class="overall-rating"]/span').text
+        # these next four are annoying because some people do not do them and the class tags are same
+        # jk, i just didn't look hard enough for the path whoohooo
+        ease = review.find_element_by_xpath('.//span[@class="reviews-stars  rating-ease-of-use"]/span[@class="milli  rating-decimal"]/span[1]').text
+        feature = review.find_element_by_xpath('.//span[@class="reviews-stars  rating-features"]/span[@class="milli  rating-decimal"]/span[1]').text
+        support = review.find_element_by_xpath('.//span[@class="reviews-stars  rating-customer-service"]/span[@class="milli  rating-decimal"]/span[1]').text
+        value = review.find_element_by_xpath('.//div[@class="cell  three-twelfths  reviews-col columns4 lap-three-twelfths  palm-one-half"]/span[@class="reviews-stars  rating-value"]/span[@class="milli  rating-decimal"]/span[1]').text
+        recommendation = review.find_element_by_xpath('.//img[@class="gauge-svg-image"]').get_attribute('alt')
     
     # the reviews themselves
-    
+    # for loop to iterate through the p tags and put them in correct element
+    test = driver.find_element_by_xpath('//div[@class="review-comments  color-text"]/p[1]')
+    re.search('//b'.text, 'Comments: ?')
+    if test.find_element_by_xpath('./b') == 'Comments:' or test.find_element_by_xpath('./b') == 'Comments: ':
+        # use regex for the conditional above instead
+        comment = test.text
+    # comments = driver.find_elements_by_xpath('div[@class="review-comments  color-text"]/p[1]')
+    # pros = driver.find_elements_by_xpath('div[@class="review-comments  color-text"]/p[2]')
+    # cons = driver.find_elements_by_xpath('div[@class="review-comments  color-text"]/p[3]')
 
-    for support in supports:
-        print(support.text)
-    print('-'*40)
-    for value in values:
-        print(value.text)
-
-    print(len(supports), 'supports length')
-    print(len(values), 'values length')
 
     # make a dictionary that will eventually be outputted
     # reviews_dict = {}
